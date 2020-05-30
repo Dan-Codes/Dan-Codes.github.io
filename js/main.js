498310da9f4bec61484afccd3444553c9ba23665
dan-codes <dli123@syr.edu>
2020-05-30 02:09:57 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var transitionIdentifiers = [
+  ".menuHeading",
+  ".item",
+  ".menuDivider",
+  ".leftTransition",
+  ".rightTransition",
+  ".fadeIn",
+];
+
+transitionIdentifiers.forEach(function (identifier) {
+  $(identifier).appear();
+  $(identifier).css("-webkit-transition", "2s");
+  $(identifier).on("appear", function (e, $all_appeared_elements) {
+    $(this).addClass("visible"); // this code is executed for each appeared element
+  });
+});
+
+$.force_appear();
